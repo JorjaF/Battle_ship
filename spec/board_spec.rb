b8c1fdab80ba62ade604a97b9525409c2b7c4392
Zanna Fitch <zannafitch2004@gmail.com>
2023-05-27 14:00:12 -0500

    add board class with valid coordinates

---
+++ spec/board_spec.rb
@@ -0,0 +1,21 @@
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+
+RSpec.describe Board do
+  it "exists and has attributes/cells" do
+    board = Board.new
+
+    expect(board).to be_an_instance_of(Board)
+    expect(board.cells).to be_a (Hash)
+  end
+  it "can validate coordinates" do
+    board = Board.new
+
+    expect(board.valid_coordinate?("A1")).to eq(true)
+    expect(board.valid_coordinate?("D4")).to eq(true)
+    expect(board.valid_coordinate?("A5")).to eq(false)
+    expect(board.valid_coordinate?("E1")).to eq(false)
+    expect(board.valid_coordinate?("A22")).to eq(false)
+  end
+end
